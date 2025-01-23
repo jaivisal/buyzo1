e3cc9df5d97c344c30f0edeb4e903c763b37ef7e
jaivisal <138554017+jaivisal@users.noreply.github.com>
2025-01-23 21:35:44 +0530

    minor fix

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
-import { assets } from '../assets/assets';
+import { assets } from '../assets/asset';
 import Title from '../components/Title';
 import ProductItem from '../components/ProductItem';
 
